c290a44b2f52d1c19f3f1573623f73d4aa6e4a85
sunchao <sunchao7610@qq.com>
2023-05-13 15:57:24 +0800

    添加基本的运行环境

--- helloword.cpp
+++ helloword.cpp
@@ -1,7 +1,17 @@
-#include <vector>
-#include <cstdio>
+#include<bits/stdc++.h>
+using namespace std;
+
+void print( int x ){
+    cout << x << " ";
+}
 
 int main()
 {
-	cout << "helloware" << endl;
+	cout << "Hello World!" << endl;
+	for (int i = 1; i <= 10; i++)
+	{
+		print(i);
+	}
+	system("pause");
+	return 0;
 }
